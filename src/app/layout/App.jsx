d29c8d16fea711fd8c4bf7b1032eca4af87af2e0
Chris Dalao <chrisdalao@Chriss-MBP.lan>
2022-11-29 21:27:46 -0800

    form can be opened and closed

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,17 @@
+import React, { Fragment, useState } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
 
-function App() {
+export default function App() {
+  const [formOpen, setFormOpen] = useState(false);
+
   return (
-    <div className="App">
-     <h1>Join-Up</h1>
-    </div>
+    <>
+      <NavBar setFormOpen={setFormOpen}/>
+      <Container className="main">
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen}/>
+      </Container>
+    </>
   );
 }
-
-export default App;
